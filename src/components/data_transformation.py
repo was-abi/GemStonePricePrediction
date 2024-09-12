6d73bc7ccc8b5a4001ac839a7144dc3160f02357
was-abi <abhi123gajul@gmail.com>
2024-09-12 01:45:24 -0400

    Components, data ingestion,data transformation,model trainer, model evaluation, training_pipeline.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -7,18 +7,16 @@ import sys
 from dataclasses import dataclass
 from pathlib import Path
 
-
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler 
 
 from src.utils.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
-
 
 class DataTransformation:
     def __init__(self):
--- test.py
+++ test.py
@@ -9,3 +9,4 @@ logging.info(" this my second tesgting")
 logging.info(" this my second tesgting")
 logging.info(" this my second tesgting")
 logging.info(" this my second tesgting")
+
